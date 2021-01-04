47c0d38f540437952337fb90c8e64436ee3cf6fe
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-04 16:36:01 +0100

    css added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 import * as THREE from 'three';
 
-import {light} from './lightbase';
-import {light2} from './lightdirect';
-import {camera} from './camerabase';
-import {loadManager, objLoader, mtlLoader} from './loadingModule';
-import {controls} from './controlsModule';
+import { light } from './lightbase';
+import { light2 } from './lightdirect';
+import { camera } from './camerabase';
+import { loadManager, objLoader, mtlLoader } from './loadingModule';
+import { controls } from './controlsModule';
 
 'use strict';
 
@@ -16,10 +16,75 @@ function main() {
   const heightRatio = 1;
   canvas.height = canvas.width * heightRatio;
   const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-      alpha: true
+    canvas,
+    antialias: true,
+    alpha: true
   });
   renderer.setClearColor(0x000000, 0);
-  
+
+  controls.update(true);
+
+  const scene = new THREE.Scene();
+
+  camera.add(light);
+  scene.add(camera);
+
+  scene.add(light2);
+  scene.add(light2.target);
+
+  let tytul = 'aldechydy_772.mtl'
+
+  mtlLoader.load(tytul, (materials) => {
+    //materials.depthWrite = false;
+    //materials.side = THREE.BackSide;
+    //materials.side = THREE.FrontSide;
+    materials.preload()
+    objLoader.setMaterials(materials);
+    objLoader.load('aldechydy_772.obj', (object) => {
+
+      scene.add(object);
+    });
+  });
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function animate() {
+
+    requestAnimationFrame(animate);
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
+
+    renderer.render(scene, camera);
+
+  }
+
+  function render() {
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    renderer.sortObjects = false;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+  animate();
+
 }
+
+main();

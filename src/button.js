98c7981bfaad3a9ee6229b5b1e15c22d83ceb0a1
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-17 20:21:47 +0100

    new 3d model

--- src/button.js
+++ src/button.js
@@ -4,10 +4,10 @@ export const buttonChange = document.querySelector("#d");
 
 export const buttonClick = () => {
     if (tytul === 'aldechydy_772.mtl') {
-        tytul = 'aldechydy_772_b.mtl';
-        tytulobj = 'aldechydy_772_b.obj';
+        tytul = 'aldechydy_772_b4.mtl';
+        tytulobj = 'aldechydy_772_b4.obj';
     }
-    else if (tytul ==='aldechydy_772_b.mtl' )
+    else if (tytul ==='aldechydy_772_b4.mtl' )
      {
         tytul = 'aldechydy_772.mtl';
         tytulobj = 'aldechydy_772.obj';
